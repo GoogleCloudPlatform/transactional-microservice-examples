1c6f903c72b25c2c66410c37b7847ba8f32a4992
Etsuji Nakai <enakai@google.com>
2020-12-09 03:33:51 -0500

    Update: remove frontend temporary.

--- frontend/lib/entity/memu.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:freezed_annotation/freezed_annotation.dart';
-
-part 'memu.freezed.dart';
-
-@freezed
-abstract class Menu with _$Menu {
-  const factory Menu({String name, Icon icon}) = _Menu;
-}
